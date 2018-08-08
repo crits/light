cb301da99b3e951a9ccaf12af4290042ef3d8704
Coleman Kane <ckane@colemankane.org>
2018-08-08 00:16:34 -0400

    Add modular implementation of resources
    Used the pkgutil iter_modules() trick to load modules dynamically from
    a folder. The folder merely needs to contain an empty __init__.py file,
    and modules can be added/removed from the application by adding them to
    this folder (and no need to manually name them in __init__.py).
    
    I decided to name these modules "photons" as the Falcon framework makes
    them pretty lean. There is support in the pkgutil, I think, for
    recursive directory discovery using pkgutil.walk_modules(), so that
    could open up more organizational options for us.

--- light/framework.py
+++ light/framework.py
@@ -1,4 +1,6 @@
 import falcon
+import pkgutil
+import sys
 
 from .api import _test_resource_get
 
@@ -7,10 +9,35 @@ class TestResource(object):
         """Handles all GET requests."""
         _test_resource_get(req, res)
 
-
 def route_framework(app):
     # Instantiate the TestResource class
     test_resource = TestResource()
+
+    # Dynamically find modules
+    # Adapted from: https://stackoverflow.com/questions/1057431/how-to-load-all-modules-in-a-folder
+    for importer, pkgname, ispkg in pkgutil.iter_modules(['./light/photons']):
+        # Build the "full package name" by joining the modules dir name
+        # with the module file name (minus .py)
+        full_pkg_name = 'photons.{pkgname}'.format(pkgname=pkgname)
+
+        # Want to make sure we haven't already loaded this module, this tests that
+        if full_pkg_name not in sys.modules:
+            module = importer.find_module(full_pkg_name).load_module(full_pkg_name)
+
+            # Each module contains a variable named 'resources' that is a list of
+            # strings naming classes that are to be integrated as Falcon resources.
+            # Each of these *must* have a function routes_set(app) defined, and the
+            # requisite routing code belongs there.
+            for class_name in module.resources:
+                # Connect the class object to a variable
+                cobj = getattr(module, class_name)
+
+                # Then we can instantiate an instance of the class
+                cinst = cobj()
+
+                # And finally, set up routing by calling the method I defined earlier.
+                # TODO: Maybe this could be a class function? TBD.
+                cinst.routes_set(app)
 
     # Add a route to serve the resource
     app.add_route('/test', test_resource)
